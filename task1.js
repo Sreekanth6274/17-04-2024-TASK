5283398b1157e5ea6e6467e2c281df7783f60c68
Sreekanth6274 <164642286+Sreekanth6274@users.noreply.github.com>
2024-05-18 11:06:31 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,28 @@
+// Javascript Functions
+
+
+// Function Declaration
+
+
+function calculations(){ 
+    let num1 = 1200;
+    let num2 = 1800;
+    let values =` Total Values = ${num1} + ${num2}`
+    console.log(values)
+}
+calculations();
+
+// Function Expressions
+
+let personalInfo = function(){
+   let  firstName =" Sreekanth";
+     let  lastName = "Peddarapu";
+     let details = `First name ${firstName} Last Name ${lastName}`
+    console.log(details);
+}
+personalInfo();
+
+function msg(){
+document.writeln("sreekanth")
+}
+msg();
